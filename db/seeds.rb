fb03f268ad9d997b698bad4f749e0f3ed598f9f5
Karl Keller <kgk86@hotmail.com>
2023-09-26 14:22:29 +0200

    Create Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    name: Faker::Name.name
+  )
+
+  Post.create(
+    user: user,
+    title: Faker::Movie.title,
+    content: Faker::Movie.quote
+  )
+end
